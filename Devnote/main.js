891a6578b28e220680ec6b40f31a55d53b937e73
KUflower <94sangmi@gmail.com>
2016-02-01 15:58:42 +0900

    clean index.html

--- Devnote/main.js
+++ Devnote/main.js
@@ -23,7 +23,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 1000, height: 700});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname +'/html/mail/inbox.html');
+  mainWindow.loadURL('file://' + __dirname +'/html/mail/index.html');
   // '/html/mail/inbox.html'
 
   // Open the DevTools.
