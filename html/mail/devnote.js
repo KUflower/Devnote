909ef81115cd3410b0b3a5bc665294f6417796dc
NaInyoup <windy9898@daum.net>
2016-10-08 22:07:12 +0900

    Update create, save, delete function & Fix bugs

--- html/mail/devnote.js
+++ html/mail/devnote.js
@@ -1,234 +1,157 @@
-function defaultselection() {
+var Promise = require('bluebird')
+var fs      = Promise.promisifyAll(require('fs'))
+var path    = require('path')
 
-    document.getElementById("notelist").innerHTML = "";
-    var filesystem = require('fs');
-    var dir = __dirname + '/devfolder/';
-    filesystem.readdirSync(dir).forEach(function(file) {
-        file = dir + '/' + file;
-        var stat = filesystem.statSync(file);
-        if (stat && stat.isDirectory()) {
-            var filename = file.replace(/^.*[\\\/]/, '');
-            var dir2 = dir + filename + '/';
-            filesystem.readdirSync(dir2).forEach(function(file) {
-                file = dir2 + '/' + file;
-                stat = filesystem.statSync(file);
-                var filename2 = file.replace(/^.*[\\\/]/, '');
+String.prototype.format = function(){
+    var t = this+''
+    for(var i = 0; i < arguments.length; i++){
+        t = t.replace(/\{[a-z|A-Z]+\}/, arguments[i]+'')
+    }
+    return t
+}
 
-                if (filename2 != '.DS_Store') {
-                    // read json
-                    var fs = require('fs');
-                    var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
+function showNoteList(notebookName){
+    $('#notelist').html('')
 
-                    var title = obj["title"];
-                    if (title.length == 0)
-                        title = 'untitled';
+    var rootDir = path.join(__dirname, 'devfolder')
 
-                    var updated = obj["updated_time"];
-                    updated = updated.slice(4, updated.length);
-                    updated = updated.slice(0, -7);
+    var notebookList = []
+    if(notebookName === '모든 노트'){
+        notebookList = fs.readdirSync(rootDir)
+    } else {
+        notebookList = [ notebookName ]
+    }
+    notebookList.forEach(function(notebook) {
+        var notebookDir = path.join(rootDir, notebook)
+        var stat = fs.statSync(notebookDir)
+        if(!stat.isDirectory()){ return }
 
-                    var summary = obj["content"];
-                    var maxl = summary.length;
-                    if (maxl > 40)
-                        maxl = 40;
-                    summary = summary.slice(0, maxl);
+        fs.readdirSync(notebookDir).forEach(function(note){
+            var extType = note.replace(/[^.]*\./, '')
+            if(extType === '.DS_Store'){ return }
 
-                    var notelistelement = "<a href='#' class='list-group-item' onclick=shownote('" + file + "')> <h5>" + filename + "</h5> <h4>" + title + "</h4> <p class = 'hidden-xs hidden-sm'>" + summary + "</p> <div class = 'stick-top-right small-padding text-default-light text-sm'>" + updated + "</div></a>";
-                    document.getElementById("notelist").innerHTML += String(notelistelement);
-                }
-            });
-        }
-    });
+            var noteDir = path.join(notebookDir, note)
+
+            var noteObj = JSON.parse(fs.readFileSync(noteDir, 'utf8'))
+            var title   = noteObj.title || 'untitled'
+            var updated = noteObj.updated_time.slice(4, -7) || ''
+            var summary = noteObj.content.replace(/<[^\/].*?\/>/, '').substring(0, 40)
+            noteDir = noteDir.replace(/\\/g, '\\\\')
+
+            var noteListHtml = 
+                `<a href="#" class="list-group-item" onclick="showNote('{noteDir}')">
+                    <h5>{notebook}</h5>
+                    <h4>{title}</h4>
+                    <p class="hidden-xs hidden-sm">{summary}</p>
+                    <div class="stick-top-right small-padding text-default-light text-sm">{updated}</div>
+                </a>`
+            $("#notelist").append(noteListHtml.format(noteDir, notebook, title, summary, updated))
+        })
+    })
 }
 
 function showNotebookList() {
-    var filesystem = require('fs');
-    var dir = __dirname + '/devfolder/';
 
-    document.getElementById("select-notebook").innerHTML = "<option>모든 노트</option>";
-    filesystem.readdirSync(dir).forEach(function(file) {
-        file = dir + '/' + file;
-        var stat = filesystem.statSync(file);
+    var rootDir = path.join(__dirname, 'devfolder')
+    var notebookList = $("#select-notebook")
+    notebookList.html('<option>모든 노트</option>')
+
+    fs.readdirSync(rootDir).forEach(function(notebook) {
+        var notebookPath = path.join(rootDir, notebook)
+
+        var stat = fs.statSync(notebookPath);
         if (stat && stat.isDirectory()) {
-            var filename = file.replace(/^.*[\\\/]/, '');
-            document.getElementById("select-notebook").innerHTML += "<option>" + filename + "</option>";
+            var notebookListHtml = "<option>{notebookName}</option>"
+            notebookList.append(notebookListHtml.format(notebook))
         }
-    });
+    })
 
-    // 모든 노트일 때
-    var x = document.getElementById("select-notebook").value;
-    console.log(x);
-    if (x == "모든 노트") {
-
-        defaultselection();
-    }
+    var notebookName = $('#select-notebook').val()
+    return showNoteList(notebookName)
 }
 
-function showselected() {
-    var x = document.getElementById("select-notebook").value;
-    var filesystem = require('fs');
-    if (x == "모든 노트") {
+function showNote(noteDir) {
 
-        defaultselection();
-    } else {
-        dir = __dirname + '/devfolder/' + x + '/';
-        document.getElementById("notelist").innerHTML = "";
-        filesystem.readdirSync(dir).forEach(function(file) {
-            file = dir + '/' + file;
-            stat = filesystem.statSync(file);
-            filename = file.replace(/^.*[\\\/]/, '');
-            if (filename != '.DS_Store') {
+    var buf = fs.readFileSync(noteDir, 'utf8')
+    if(!buf){ return alert('해당 노트를 불러올 수 없습니다.') } // Handle FileLoading Error
 
-                // read json
-                var fs = require('fs');
-                var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
+    try{ var obj = JSON.parse(buf) } // Handle JSON Parse Error
+    catch(err){ return alert ('해당 파일의 형식이 올바르지 않습니다.') }
 
-                var title = obj["title"];
-                if (title.length == 0)
-                    title = 'untitled';
-
-                var updated = obj["updated_time"];
-                updated = updated.slice(4, updated.length);
-                updated = updated.slice(0, -7);
-
-                var summary = obj["content"];
-                var maxl = summary.length;
-                if (maxl > 40)
-                    maxl = 40;
-                summary = summary.slice(0, maxl);
-
-                document.getElementById("notelist").innerHTML += "<a href='#' class='list-group-item' onclick=shownote('" + file + "')> <h5>" + x + "</h5> <h4>" + title + "</h4> <p class = 'hidden-xs hidden-sm'>" + summary + "</p> <div class = 'stick-top-right small-padding text-default-light text-sm'>" + updated + "</div></a>";
-            }
-        });
-    }
-
-    // if there is no note, show null
-    if (document.getElementById("notelist").innerHTML == "") {
-
-        //				document.getElementById("notelist").innerHTML+="<a href='#' class='list-group-item' onclick=shownote('"+file+"')> <h5>Notebook</h5> <h4>"+"null"+"</h4> <p class = 'hidden-xs hidden-sm'>Summary</p> <div class = 'stick-top-right small-padding text-default-light text-sm'>10:30pm</div></a>";
-    }
-}
-
-function shownote(file) {
-
-    var fs = require('fs');
-    var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
-
-    var editor = document.getElementById("markdown-editor").contentWindow.editor;
-    editor.setValue(obj["content"]);
-
-    var notetitle = document.getElementById("note-title");
-    notetitle.value = obj["title"];
-
-    var selectnote = document.getElementById("select-language");
-    selectnote.value = obj["type"];
-
-}
-
-function test(d) {
-    console.log('hi' + d);
+    document.getElementById("note-title").value = obj.title || '알 수 없음'
+    document.getElementById("origin-note-path").value = noteDir
+    document.getElementById("select-language").value = obj.type
+    document.getElementById("markdown-editor").contentWindow.editor.setValue(obj.content)
 }
 
 function saveNote() {
-    var notebook = document.getElementById("select-notebook").value;
-    var dir = __dirname + '/devfolder/' + notebook;
-    var fs = require('fs'); //file write
-    // 노트북 디렉토리 없을 시 폴더 생성
-    console.log(notebook);
-    if (notebook == "모든 노트") {
-        alert('모든 노트 모아보기 기능에서는 노트 저장 불가합니다');
-        return;
+
+    var notebookName = $('#select-notebook').val()
+    var notebookDir = path.join(__dirname, 'devfolder', notebookName)
+
+    if(notebookName === "모든 노트"){
+        return alert('모든 노트 모아보기 기능에서는 노트 저장 불가합니다');
     }
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+    if(!fs.existsSync(notebookDir)){
+        fs.mkdirSync(notebookDir)
     }
-    var editor = document.getElementById("markdown-editor").contentWindow.editor;
-    console.log(editor.getValue());
-    var myData = {
-        version: '1.0'
-    };
-    // 파일 이름에 Time stamp
-    var dt = new Date();
-    var utcDate = dt.toUTCString();
-    if (!Date.now) {
-        Date.now = function() {
-            return new Date().getTime();
-        }
+
+    var date = new Date()
+    var noteTitle = $('#note-title').val() || 'untitled'
+    var noteData = {
+        version     : '1.0',
+        title       : noteTitle,
+        created_time: ~~(date.getTime() / 1000),
+        updated_time: date.toUTCString(),
+        type        : $('#select-language').val(),
+        important   : false,
+        trash       : false,
+        content     : document.getElementById("markdown-editor").contentWindow.editor.getValue()
     }
-    var timestamp = Math.floor(Date.now() / 1000);
-    var title = document.getElementById('note-title').value;
-    if (title.length == 0)
-        title = 'untitled';
-    myData["title"] = title;
-    myData["created_time"] = timestamp;
-    myData["updated_time"] = utcDate;
-    myData["type"] = document.getElementById('select-language').value;
-    myData["important"] = false;
-    myData["trash"] = false;
-    myData["content"] = editor.getValue();
-    //			var outputFilename = __dirname + '/devfolder/' + String(timestamp) + '.json';
-    var outputFilename = dir + '/' + title + '.json';
-    fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("JSON saved to " + outputFilename);
-            showselected();
-        }
-    });
+
+    var noteDir = path.join(notebookDir, noteTitle+'.json')
+    var originNoteDir  = $("#origin-note-path").val()
+
+    Promise.resolve()
+    .then(function(){ if(originNoteDir){ return fs.unlinkAsync(originNoteDir) } })
+    .then(function(){ return fs.writeFileAsync(noteDir, JSON.stringify(noteData, null, 4)) })
+    .then(function(){ return $("#origin-note-path").val(noteDir) })
+    .then(function(){ return showNoteList(notebookName) })
+    .catch(function(err){ return console.log(err) })
 }
 
 function createNote() {
-    var notebook = document.getElementById("select-notebook").value;
-    var dir = __dirname + '/devfolder/' + notebook;
-    var fs = require('fs'); //file write
-    // 노트북 디렉토리 없을 시 폴더 생성
-    if (notebook == "모든 노트") {
-        alert('모든 노트 모아보기 기능에서는 노트 추가가 불가합니다');
-        return;
+    // CreateNote doesn't have to save file. Just clear the editor
+    var notebookName = $("#select-notebook").val()
+    var notebookDir = path.join(__dirname, "devfolder", notebookName)
+
+    if(notebookName == "모든 노트"){
+        return alert('모든 노트 모아보기 기능에서는 노트 추가가 불가합니다')
     }
-    if (!fs.existsSync(dir)) {
-        alert('존재하지 않는 노트북에 있습니다.');
-        return;
+    if(!fs.existsSync(notebookDir)){
+        return alert('존재하지 않는 노트북에 있습니다.')
     }
 
-    // set current page blank
-    document.getElementById('note-title').value = "";
-    var editor = document.getElementById("markdown-editor").contentWindow.editor;
-    editor.setValue("");
+    document.getElementById('note-title').value = ""
+    document.getElementById('origin-note-path').value = ""
+    document.getElementById('markdown-editor').contentWindow.editor.setValue("")
+}
 
-    var myData = {
-        version: '1.0'
-    };
-    // 파일 이름에 Time stamp
-    var dt = new Date();
-    var utcDate = dt.toUTCString();
-    if (!Date.now) {
-        Date.now = function() {
-            return new Date().getTime();
-        }
-    }
-    var timestamp = Math.floor(Date.now() / 1000);
-    var title = document.getElementById('note-title').value;
-    if (title.length == 0)
-        title = 'untitled';
-    myData["title"] = title;
-    myData["created_time"] = timestamp;
-    myData["updated_time"] = utcDate;
-    myData["type"] = document.getElementById('select-language').value;
-    myData["important"] = false;
-    myData["trash"] = false;
-    myData["content"] = editor.getValue();
-    //			var outputFilename = __dirname + '/devfolder/' + String(timestamp) + '.json';
-    var outputFilename = dir + '/' + myData["title"] + '.json';
-    fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("JSON saved to " + outputFilename);
-            showselected();
-        }
-    });
+function deleteNote(){
+    var originNoteDir = $('#origin-note-path').val()
+    if(!originNoteDir){ return }
+
+    fs.unlinkAsync(originNoteDir)
+    .then(function(){
+        document.getElementById('note-title').value = ""
+        document.getElementById('origin-note-path').value = ""
+        document.getElementById('markdown-editor').contentWindow.editor.setValue("")
+        return showNoteList($('#select-notebook').val())
+    }, function(err){
+        console.log(err)
+        return alert("노트 삭제에 실패하였습니다.")
+    })
+    .catch(console.log)
 }
 
 function createNotebook(notebook) {
@@ -238,8 +161,7 @@ function createNotebook(notebook) {
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     } else {
-        alert('이미 존재하는 노트북입니다.');
-        return;
+        return alert('이미 존재하는 노트북입니다.')
     }
 
     showNotebookList();
@@ -278,61 +200,107 @@ function createNotebook(notebook) {
             console.log(err);
         } else {
             console.log("JSON saved to " + outputFilename);
-            showselected();
+            showNoteList(notebook);
         }
     });
 }
 $(document).ready(function() {
-    //
-
+    var setMarkdownEditorCss = function(obj){
+        for(var id in obj){
+            for(var attr in obj[id]){
+                document.getElementById('markdown-editor').contentWindow
+                .document.getElementById(id).style[attr] = obj[id][attr]
+            }
+        }
+        
+    }
     $("#btn-edit").click(function() {
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("in").style.display = "inline";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("in").style.width = "50%";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.display = "inline";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.left = "50%";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.width = "auto";
+        var style = {
+            in : {
+                display : 'inline',
+                width   : '50%'
+            },
+            out : {
+                display : 'inline',
+                left    : '50%',
+                width   : 'auto'
+            } 
+        }
+        return setMarkdownEditorCss(style)
     });
     $("#btn-markdown").click(function() {
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.display = "none";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("in").style.display = "inline";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("in").style.width = "100%";
+        var style = {
+            in : {
+                display : 'inline',
+                width   : '100%'
+            },
+            out : {
+                display : 'none'
+            }
+        }
+        return setMarkdownEditorCss(style)
     });
     $("#btn-preview").click(function() {
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("in").style.display = "none";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.display = "inline";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.left = "0%";
-        document.getElementById('markdown-editor').contentWindow.document.getElementById("out").style.width = "auto";
+        var style = {
+            in : {
+                display : 'none'
+            },
+            out : {
+                display : 'inline',
+                left    : '0%',
+                width   : 'auto'
+            } 
+        }
+        return setMarkdownEditorCss(style)
     });
     $("#btn-save").click(function() {
-        var notebook = document.getElementById("select-notebook").value;
-        if (notebook != "모든 노트") {
-            saveNote();
+        var noteTitle = $('#note-title')
+        if(!noteTitle.val()){
+            return alert("노트의 제목을 입력해 주세요")    
         }
+
+        if(noteTitle.val() === '모든 노트'){
+            noteTitle.val('')
+            return alert("'모든 노트'는 노트의 제목이 될 수 없습니다.")
+        } 
+
+        return saveNote()
         // showselected();
     });
     $("#btn-download").click(function() {
         document.getElementById('markdown-editor').contentWindow.showMenu();
     });
-    $("#btn-delete").click(function() {});
+    $("#btn-delete").click(function() {
+        if(confirm("노트를 삭제하시겠습니까?")){
+            return deleteNote()
+        }
+    });
 
-    $("#dialog-6").dialog({
+    $("#dialog-6").dialog({ // BeforeClose is not good event for vaildation checking.
         autoOpen: false,
         buttons: {
             OK: function() {
+                var title = $('#notebook-title').val().trim()
+                if(!title){
+                    alert("노트북 이름이 비었습니다.")
+                    $("[for=terms]").addClass("invalid");
+
+                } else if(title === '모든 노트'){
+                    alert(title+"는 노트북 이름이 될 수 없습니다.")
+
+                } else {
+                    $(this).dialog('close')
+                }
+            },
+            Close: function() {
+                $('#notebook-title').val('')
                 $(this).dialog("close");
             }
         },
         beforeClose: function(event, ui) {
-            var x = document.getElementById("notebook-title").value;
-            if (!(/\S/.test(x))) {
-                alert("노트북 이름이 비었습니다.");
-                event.preventDefault();
-                $("[for=terms]").addClass("invalid");
-            } else if (x == "모든 노트") {
-                alert(x + " 은(는) 노트북 이름이 될 수 없습니다.");
-            } else { // create new notebook
-                createNotebook(x);
-            }
+            var title = $('#notebook-title').val().trim()
+            if(title){ return createNotebook(title) }
+
         },
         width: 600
     });
@@ -341,5 +309,8 @@ $(document).ready(function() {
     });
     //init
     showNotebookList();
-    // defaultselection();
 });
+
+function test(d) {
+    console.log('hi' + d);
+}
